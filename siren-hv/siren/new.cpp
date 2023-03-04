26cb9e09e869f6ccb225c38451a6ddbec817a59d
HyperSine <hypersine.git@outlook.com>
2023-03-04 16:30:41 +0800

    improve align arithmetic
    Signed-off-by: HyperSine <hypersine.git@outlook.com>

--- siren-hv/siren/new.cpp
+++ siren-hv/siren/new.cpp
@@ -1,6 +1,7 @@
 #include "new.hpp"
 #include "utility.hpp"
 #include <wdm.h>
+#include <ntintsafe.h>
 
 static_assert(__STDCPP_DEFAULT_NEW_ALIGNMENT__ == MEMORY_ALLOCATION_ALIGNMENT);
 
@@ -14,48 +15,50 @@ void* operator new(std::size_t size, bool paged, std::align_val_t alignment, con
     // Memory allocations of less than PAGE_SIZE are not necessarily page-aligned 
     //     but are aligned to 8-byte boundaries in 32-bit systems and to 16-byte boundaries in 64-bit systems.
     //
-
-    if (to_underlying(alignment) <= MEMORY_ALLOCATION_ALIGNMENT) {
+    if (siren::to_underlying(alignment) <= MEMORY_ALLOCATION_ALIGNMENT) {
         reserved_size = 0;
-    } else if (to_underlying(alignment) == PAGE_SIZE) {
+    } else if (siren::to_underlying(alignment) == PAGE_SIZE) {
         size = std::max<std::size_t>(size, PAGE_SIZE);     // over allocate if `size` less than page size.
         reserved_size = 0;
     } else {
-        reserved_size = to_underlying(alignment) - 1 + sizeof(void*);
+        reserved_size = siren::to_underlying(alignment) - 1 + sizeof(void*);
     }
 
     void* p;
-
-    // NTDDI_WIN10_VB, where VB stands for Vibranium which is the codename of `Windows 10, version 2004`
-    // Source: https://en.wikipedia.org/wiki/List_of_Microsoft_codenames
+    if (std::size_t allocate_size; STATUS_SUCCESS == RtlSizeTAdd(reserved_size, size, &allocate_size)) {
+        // NTDDI_WIN10_VB, where VB stands for Vibranium which is the codename of `Windows 10, version 2004`
+        // Source: https://en.wikipedia.org/wiki/List_of_Microsoft_codenames
 #if NTDDI_VERSION >= NTDDI_WIN10_VB
-    POOL_FLAGS pool_flags = paged ? POOL_FLAG_PAGED : POOL_FLAG_NON_PAGED;
+        POOL_FLAGS pool_flags = paged ? POOL_FLAG_PAGED : POOL_FLAG_NON_PAGED;
 
-    // use ExAllocatePool2 for ExAllocatePoolWithTag has been deprecated since `Windows 10, version 2004`.
-    //
-    // Memory that ExAllocatePool2 allocates is zero initialized.
-    // Kernel-mode drivers should not opt-out of zeroing for allocations 
-    //     that will be copied to an untrusted location (user-mode, over the network, etc.) to avoid disclosing sensitive information.
-    p = ExAllocatePool2(pool_flags, reserved_size + size, siren::pool_tag_v);
+        // use ExAllocatePool2 for ExAllocatePoolWithTag has been deprecated since `Windows 10, version 2004`.
+        //
+        // Memory that ExAllocatePool2 allocates is zero initialized.
+        // Kernel-mode drivers should not opt-out of zeroing for allocations 
+        //     that will be copied to an untrusted location (user-mode, over the network, etc.) to avoid disclosing sensitive information.
+        p = ExAllocatePool2(pool_flags, allocate_size, siren::pool_tag_v);
 #else
-    POOL_TYPE pool_type = paged ? PagedPool : NonPagedPoolNx;
+        POOL_TYPE pool_type = paged ? PagedPool : NonPagedPoolNx;
 
-    // Memory that ExAllocatePoolWithTag allocates is uninitialized. 
-    // A kernel-mode driver must first zero this memory if it is going to make it visible to user-mode software (to avoid leaking potentially privileged contents).
-    p = ExAllocatePoolWithTag(pool_type, reserved_size + size, siren::pool_tag_v);
-    if (p) {
-        RtlZeroMemory(p, reserved_size + size);
-    }
+        // Memory that ExAllocatePoolWithTag allocates is uninitialized. 
+        // A kernel-mode driver must first zero this memory if it is going to make it visible to user-mode software (to avoid leaking potentially privileged contents).
+        p = ExAllocatePoolWithTag(pool_type, allocate_size, siren::pool_tag_v);
+        if (p) {
+            RtlZeroMemory(p, allocate_size);
+        }
 #endif
+    } else {
+        p = nullptr;
+    }
 
     if (p && reserved_size) {
-        auto pp = reinterpret_cast<void**>((reinterpret_cast<uintptr_t>(p) + reserved_size) & ~(to_underlying(alignment) - 1));
+        auto pp = reinterpret_cast<void**>(siren::align_down(reinterpret_cast<uintptr_t>(p) + reserved_size, alignment));
         pp[-1] = p;
         p = pp;
     }
 
     // assert aligned
-    NT_ASSERT((reinterpret_cast<uintptr_t>(p) & (to_underlying(alignment) - 1)) == 0);
+    NT_ASSERT(siren::align_check(p, alignment));
 
     return p;
 }
@@ -78,7 +81,7 @@ void operator delete(void* p, std::size_t size) noexcept {
 
 void operator delete(void* p, std::align_val_t alignment) noexcept {
     if (p) {
-        if (to_underlying(alignment) <= MEMORY_ALLOCATION_ALIGNMENT || to_underlying(alignment) == PAGE_SIZE) {
+        if (siren::to_underlying(alignment) <= MEMORY_ALLOCATION_ALIGNMENT || siren::to_underlying(alignment) == PAGE_SIZE) {
             ExFreePoolWithTag(p, siren::pool_tag_v);
         } else {
             ExFreePoolWithTag(reinterpret_cast<void**>(p)[-1], siren::pool_tag_v);
--- siren-hv/siren/utility.hpp
+++ siren-hv/siren/utility.hpp
@@ -29,6 +29,48 @@ namespace siren {
     }
 #endif
 
+    template<std::unsigned_integral Ty>
+    [[nodiscard]]
+    constexpr bool align_check(Ty value, std::align_val_t alignment) noexcept {
+        size_t mask = to_underlying(alignment) - 1;
+        return (value & mask) == 0;
+    }
+
+    template<typename Ty>
+        requires std::is_pointer_v<Ty>
+    [[nodiscard]]
+    constexpr bool align_check(Ty ptr, std::align_val_t alignment) noexcept {
+        return align_check(reinterpret_cast<uintptr_t>(ptr), alignment);
+    }
+
+    template<std::unsigned_integral Ty>
+    [[nodiscard]]
+    constexpr Ty align_up(Ty value, std::align_val_t alignment) noexcept {
+        size_t mask = to_underlying(alignment) - 1;
+        return static_cast<Ty>((value + mask) & ~mask);
+    }
+
+    template<typename Ty>
+        requires std::is_pointer_v<Ty>
+    [[nodiscard]]
+    constexpr Ty align_up(Ty ptr, std::align_val_t alignment) noexcept {
+        return reinterpret_cast<Ty>(align_up(reinterpret_cast<uintptr_t>(ptr), alignment));
+    }
+
+    template<std::unsigned_integral Ty>
+    [[nodiscard]]
+    constexpr Ty align_down(Ty value, std::align_val_t alignment) noexcept {
+        size_t mask = to_underlying(alignment) - 1;
+        return static_cast<Ty>(value & ~mask);
+    }
+
+    template<typename Ty>
+        requires std::is_pointer_v<Ty>
+    [[nodiscard]]
+    constexpr Ty align_down(Ty ptr, std::align_val_t alignment) noexcept {
+        return reinterpret_cast<Ty>(align_down(reinterpret_cast<uintptr_t>(ptr), alignment));
+    }
+
     template<std::unsigned_integral Ty, size_t From, size_t To>
         requires (From < To && To <= std::numeric_limits<Ty>::digits)
     struct range_bits_t {
