cc50d8013554f131c3de355cba8f297f5d597696
HyperSine <hypersine.git@outlook.com>
2023-03-06 00:59:24 +0800

    add no-nothrow version operator new
    Signed-off-by: HyperSine <hypersine.git@outlook.com>

--- siren-hv/siren/new.hpp
+++ siren-hv/siren/new.hpp
@@ -1,12 +1,31 @@
 #pragma once
+#include <specstrings.h>
+#include <kernelspecs.h>
 #include <new>
 
 namespace siren {
     constexpr uint32_t pool_tag_v = 'vhrs';
 }
 
+_When_(paged == true, _IRQL_requires_max_(APC_LEVEL))
+_When_(paged == false, _IRQL_requires_max_(DISPATCH_LEVEL))
+[[nodiscard]]
+void* operator new(std::size_t size, bool paged, std::align_val_t alignment);
+
+_When_(paged == true, _IRQL_requires_max_(APC_LEVEL))
+_When_(paged == false, _IRQL_requires_max_(DISPATCH_LEVEL))
+[[nodiscard]]
+void* operator new(std::size_t size, std::size_t count, bool paged, std::align_val_t alignment);
+
+_When_(paged == true, _IRQL_requires_max_(APC_LEVEL))
+_When_(paged == false, _IRQL_requires_max_(DISPATCH_LEVEL))
 [[nodiscard]]
 void* operator new(std::size_t size, bool paged, std::align_val_t alignment, const std::nothrow_t&) noexcept;
+
+_When_(paged == true, _IRQL_requires_max_(APC_LEVEL))
+_When_(paged == false, _IRQL_requires_max_(DISPATCH_LEVEL))
+[[nodiscard]]
+void* operator new(std::size_t size, std::size_t count, bool paged, std::align_val_t alignment, const std::nothrow_t&) noexcept;
 
 //[[nodiscard]]
 //void* operator new[](std::size_t size, bool paged, std::align_val_t alignment, const std::nothrow_t&) noexcept;
