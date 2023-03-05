4b4289f81b044ec0d84142a1e95534d83cdf0ffe
HyperSine <hypersine.git@outlook.com>
2023-03-06 00:48:05 +0800

    remove to_underlying/unreachable, use the stl version
    Signed-off-by: HyperSine <hypersine.git@outlook.com>

--- siren-hv/siren/utility.hpp
+++ siren-hv/siren/utility.hpp
@@ -5,30 +5,6 @@
 #include <utility>
 
 namespace siren {
-#if __cpp_lib_to_underlying
-    using std::to_underlying;
-#else
-    template<typename EnumTy>
-    constexpr std::underlying_type_t<EnumTy> to_underlying(EnumTy e) noexcept {
-        return static_cast<std::underlying_type_t<EnumTy>>(e);
-    }
-#endif
-
-#if __cpp_lib_unreachable
-    using std::unreachable;
-#else
-    [[noreturn]]
-    constexpr void unreachable() noexcept {
-#if defined(__GNUC__) // GCC, Clang, ICC
-        __builtin_unreachable();
-#elif defined(_MSC_VER) // MSVC
-        __assume(false);
-#else
-#error "siren::unreachable() is not implemented."
-#endif
-    }
-#endif
-
     template<std::unsigned_integral Ty>
     [[nodiscard]]
     constexpr bool align_check(Ty value, std::align_val_t alignment) noexcept {
